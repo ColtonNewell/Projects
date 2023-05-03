6b3c19501894c59fe84170eefe03dec53b48bfef
ColtonNewell <113714005+ColtonNewell@users.noreply.github.com>
2023-05-03 12:54:47 -0700

    Update pdfmetadata.py

--- Scripts/pdfmetadata/pdfmetadata.py
+++ Scripts/pdfmetadata/pdfmetadata.py
@@ -1,5 +1,3 @@
-#WORK IN PROGRESS
-
 import pikepdf
 import os
 import argparse
@@ -7,18 +5,33 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('Directory', help='Directory containing PDF files to extract metadata from')
+parser.add_argument('-r','--results', help='Name of the file to output results')
 args = parser.parse_args()
 
-path = r'c:\directory\for\testing'
+dir = args.Directory
 
+count = 0
 
-if args.Directory:
-    f = open(r'c:\directory\for\testing\results.txt', 'a')
-    for file in os.listdir(path):
-        print(file)
+for path in os.listdir(dir):
+    if os.path.isfile(os.path.join(dir, path)):
+        if path.endswith('.pdf'):
+            count += 1            
+print('Processing', count, 'files...')
+
+if args.Directory and args.results:
+    f = open(args.results, 'a')
+    for file in os.listdir(dir):
         if file.endswith('.pdf'):
-            with pikepdf.open(os.path.join(path, file)) as pdf:
+            with pikepdf.open(os.path.join(dir, file)) as pdf:
                 meta = pdf.open_metadata()
-                f.write('** ' + file + ' **' + '\n' + str(meta) + '\n')
-  
+                f.write('@@ ' + file + ' @@' + '\n' + str(meta) + '\n')
     f.close
+
+else:
+    for file in os.listdir(dir):    
+        if file.endswith('.pdf'):
+            with pikepdf.open(os.path.join(dir, file)) as pdf:
+                meta = pdf.open_metadata()
+                print('@@ ' + file + ' @@' + '\n' + str(meta) + '\n')
+
+print('Done!')
